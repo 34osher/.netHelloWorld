b02782b2d8ef8a9a5f633357a95684f29330c123
osher.hacham <34osher@gmail.com>
2023-12-11 14:27:55 +0200

    ori homo

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.WriteLine("your name is ori bolbol");
         //var name = Console.ReadLine();
         var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.WriteLine($"{Environment.NewLine}Hello, ori, on {currentDate:d} at {currentDate:t}!");
         //Console.Write($"{Environment.NewLine}Press any key to exit...");
         //Console.ReadKey(true);
     }
